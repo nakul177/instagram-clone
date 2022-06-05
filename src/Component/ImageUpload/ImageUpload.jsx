163bcbbe4df8428264fb638085c18f6163129f03
nakul177 <96129513+nakul177@users.noreply.github.com>
2022-06-05 16:54:16 +0530

    upload file working now

--- src/Component/ImageUpload/ImageUpload.jsx
+++ src/Component/ImageUpload/ImageUpload.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { Button } from "@mui/material";
 import { db, storage } from "../../config/firebase";
-import firebase  from "firebase"
+import firebase from "firebase";
 
-export const ImageUpload = ({username}) => {
+export const ImageUpload = ({ username }) => {
   const [image, setImage] = useState(null);
   const [progress, setProgress] = useState(0);
   const [caption, setCaption] = useState("");
@@ -16,39 +16,41 @@ export const ImageUpload = ({username}) => {
 
   const handleUpload = () => {
     const uploadTask = storage.ref(`images/${image.name}`).put(image);
-    uploadTask.on("state_changed", (snapshot) => {
-      const progress = Math.round(
-        (snapshot.bytesTransferred / snapshot.totalBytes)*100
-      )
-      setProgress(progress)
-    },
-    (err) =>{
-      console.log(err)
-      alert(err.message)
-    },
-   () =>{
-     storage
-     .ref("image")
-     .child(image.name)
-     .getDownloadURL()
-     .then(url =>{
-       db.collection("posts").add({
-         timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-         caption:caption,
-         imageUrl:url,
-         username:username
-       });
-       setProgress(0);
-       setCaption("")
-       setImage(null)
-     })
-   } 
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setProgress(progress);
+      },
+      (err) => {
+        console.log(err);
+        alert(err.message);
+      },
+      () => {
+        storage
+          .ref("images")
+          .child(image.name)
+          .getDownloadURL()
+          .then((url) => {
+            db.collection("posts").add({
+              timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+              caption: caption,
+              imgUrl: url,
+              username: username,
+            });
+            setProgress(0);
+            setCaption("");
+            setImage(null);
+          });
+      }
     );
   };
 
   return (
     <div>
-      <progress  value={progress} max="100"/>
+      <progress value={progress} max="100" />
       <input
         type="text"
         placeholder="Enter a caption..."
